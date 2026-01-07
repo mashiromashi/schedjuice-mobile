78593a57fbb452ee5d7311c97b0d3b3372eefda6
Htin Wana <htinwana.dev@gmail.com>
2026-01-07 20:59:35 +0700

    Hopefully this doesn't crash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,11 @@ import { PortalHost } from '@rn-primitives/portal';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { toastConfig } from '@/components/toast-config';
+import ToastManager from 'toastify-react-native';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/query-client';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -16,10 +21,17 @@ export default function RootLayout() {
   const { colorScheme } = useColorScheme();
 
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
+    <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+            <Stack screenOptions={{ headerShown: false }} />
+            <PortalHost />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </GestureHandlerRootView>
+      <ToastManager config={toastConfig} visibilityTime={2000} position="top" />
+    </>
   );
 }
